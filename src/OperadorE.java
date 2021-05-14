5892278a9d7feebf16927bdd06a11157a4b6bede
Flávia Sayuri <sayuri.flavia@hotmail.com>
2021-05-14 20:16:37 -0300

    Implementado o operador E

--- src/OperadorE.java
+++ src/OperadorE.java
@@ -12,7 +12,12 @@ public class OperadorE {
 		System.out.println("Por favor, digite sua senha:");
 		senha = leitor.next();	
 		//Aqui é preciso criar um if que verifique as duas condições para o login ser bem sucedido
-		
+		//para comparar string usa-se .equals. Porém se for digitado senha com letra maiúscula ele irá dar como erro. Para todos os casos usa-se equals.IgnoreCase. Para números inteiros seria para comparar senha == 123, invés de senha.equals("123").
+		if(usuario.equalsIgnoreCase("administrador") && senha.equals("123")) {
+			System.out.println("Login realizado com sucesso!");
+		}else {
+			System.out.println("Usuário ou senha incorretos!");
+		}
 		
 		
 		leitor.close();
